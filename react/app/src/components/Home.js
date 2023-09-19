abcb975c0242150a165d431aa9c4415de475a3a7
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-09-19 15:49:48 +0200

    first connect react with the spring boot

--- react/app/src/components/Home.js
+++ react/app/src/components/Home.js
@@ -1,5 +1,18 @@
+import { useState, useEffect } from "react";
+
 export const Home = () => {
+
+    const [name, setName] = useState('default name');
+    console.log('name: ', name);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/name")
+            .then(response => response.json())
+            .then((result) => { setName(result.name);              
+            })
+    }, []);
+
     return (
-        <h1>Hello from Home</h1>
+        <h1>Hello from {name}</h1>
     );
 };
--- spring-boot/src/main/java/com/dobri/springboot/Application.java
+++ spring-boot/src/main/java/com/dobri/springboot/Application.java
@@ -2,12 +2,27 @@ package com.dobri.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping("/name")
+	public Map<String, String> getName() {
+		Map<String, String> response = new HashMap<>();
+		response.put("name", "Pestho Goshev");
+		return response;
+	}
 }
