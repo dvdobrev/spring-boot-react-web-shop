466ca32b87b38a7805e20318d58cb5dedd4e2a24
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-09-19 14:11:28 +0200

    just added one TODO to test the branch

--- spring-boot/src/main/java/com/dobri/springboot/Application.java
+++ spring-boot/src/main/java/com/dobri/springboot/Application.java
@@ -3,6 +3,8 @@ package com.dobri.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO: make the apis
+
 @SpringBootApplication
 public class Application {
 
