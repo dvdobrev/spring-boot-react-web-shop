404fe3320bfb80a8d9e6f424c1ac3807f666b093
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-09-19 14:23:49 +0200

    another test

--- spring-boot/src/main/java/com/dobri/springboot/Application.java
+++ spring-boot/src/main/java/com/dobri/springboot/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //TODO: make the apis
+//TODO: Another try
 
 @SpringBootApplication
 public class Application {
