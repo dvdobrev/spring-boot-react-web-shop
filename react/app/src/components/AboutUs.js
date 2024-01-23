8f154bcab335d2cd0508a855c44b37347c0c6542
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2024-01-23 15:08:45 +0100

    add styling to the about us page

--- react/app/src/components/AboutUs.js
+++ react/app/src/components/AboutUs.js
@@ -1,18 +1,14 @@
 import shopingcartCSS from "../components/shoppingCart/shopincart.module.css";
+import homeCSS from "../components/home/home.module.css";
 
 
 export const AboutUs = () => {
     return (
-        <div>
+        <div className={`${homeCSS["home-section"]}`}>
             <h1 className={`${shopingcartCSS["text"]}`}>About Us</h1>
             <h3 className={`${shopingcartCSS["text"]}`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nulla fugiat fuga laboriosam consequatur suscipit molestias doloribus temporibus necessitatibus odit quae est delectus, modi quaerat incidunt voluptatem ipsum velit ipsam. Ea facilis ratione accusantium quisquam nisi nam officiis fuga nemo, doloremque soluta aliquam cumque harum dolores, eveniet, maiores iusto odio.
             </h3>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
         </div>
     );
 };
