f97efdde9a55e075ad7eba57c22e5af90dcf2ce5
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2024-01-23 11:07:23 +0100

    code refactoring

--- spring-boot/src/main/java/com/dobri/springboot/Application.java
+++ spring-boot/src/main/java/com/dobri/springboot/Application.java
@@ -1,14 +1,11 @@
 package com.dobri.springboot;
 
-import com.dobri.springboot.invoice.CreateInvoice;
-import com.dobri.springboot.items.Items;
+
 import com.dobri.springboot.items.ItemsService;
 import com.dobri.springboot.user.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,37 +28,11 @@ public class Application {
         this.itemsService = itemsService;
     }
 
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-
-//    @CrossOrigin(origins = Constants.REACT_URL)
-//    @RequestMapping(value = {"/clothes/details/{id}", "/clothes/edit/{id}"}, method = RequestMethod.GET)
-//    public Clothes findItemById(@PathVariable int id) {
-//        Optional<Clothes> entity = clothesService.findItemById(id);
-//
-//        return entity.orElse(null);
-//    }
-
-
-
-//    @CrossOrigin(origins = "http://localhost:3000")
-//    @PutMapping("/data")
-//    public void editName(@RequestBody Map<String, String> request) {
-//        String newName = request.get("name");
-//        System.out.println(request.get("name"));
-//
-//        responseName.put("name", newName);
-//    }
-
-//    }
-//
 
 }
 
